a2cc01411fe9324e86d148648ffcc617e646e344
Vazgen Soghoyan <sogvazulchik@gmail.com>
2023-05-09 15:29:20 +0500

    better anchors + button to go up

--- scripts/anchors.js
+++ scripts/anchors.js
@@ -1,14 +1,30 @@
+// finding every element that can give us an anchor
 const anchors = document.querySelectorAll(".header__nav a.this-page, .welcome__links a");
 
+// giving those elements an event listeners
 anchors.forEach(anchor => {
   anchor.addEventListener("click", function(event) {
+    // canceling the usual behavior when clicking on such an element
     event.preventDefault();
 
+    // finding the element to which we should go
     const id = anchor.getAttribute("href");
     const elem = document.querySelector(id);
 
+    // the height of the header is considered
+    let shift = 0;
+
+    if (window.innerWidth > 550) {
+      shift = 64;
+    } else if (window.innerWidth > 380) {
+      shift = 43;
+    } else {
+      shift = 40;
+    }
+
+    // smoothly scrolling to the element
     window.scroll({
-      top: elem.offsetTop - 70,
+      top: elem.offsetTop - shift,
       behavior: 'smooth'
     });
   })
